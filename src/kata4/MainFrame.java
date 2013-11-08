608c374011fa893756026b0c3c12abef628101bf
usuario <usuario@10.22.146.206>
2013-11-08 13:24:23 +0000

    Versión 02

--- src/kata4/MainFrame.java
+++ src/kata4/MainFrame.java
@@ -22,6 +22,7 @@ public class MainFrame extends JFrame {
     }
 
     private void createComponents() {
+        this.add(createMoneyDialog());
         this.add(createToolbar(), BorderLayout.SOUTH);
     }
 
@@ -63,6 +64,14 @@ public class MainFrame extends JFrame {
     
     private void calculate(){
         System.out.println("Calculating...");
+        
+    }
+
+    private JPanel createMoneyDialog() {
+        JPanel panel = new JPanel();
+        panel.add(new MoneyPanel());
+        panel.add(new CurrencyPanel());
+        return panel;
         
     }
     
